4c0e81345d926b54954467492a2f7dfe86b9dffb
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-20 10:38:47 +0300

    domecontentloaded Lab DONE

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const p = document.querySelector("p")
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("The Dom has loaded");
+   p.textContent = "This is really cool!"
+
+})    
+
